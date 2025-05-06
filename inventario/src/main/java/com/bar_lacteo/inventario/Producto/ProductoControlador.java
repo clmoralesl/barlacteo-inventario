82957732ed84b83778185d0b0c5aed24e9de0284
JbastianH <joe.arancibia@duocuc.cl>
2025-05-06 19:22:07 -0400

    codigo de producto controlador

--- inventario/src/main/java/com/bar_lacteo/inventario/Producto/ProductoControlador.java
+++ inventario/src/main/java/com/bar_lacteo/inventario/Producto/ProductoControlador.java
@@ -1,5 +1,20 @@
 package com.bar_lacteo.inventario.Producto;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/producto")
 public class ProductoControlador {
+    private final ProductoRepositorio productoRepositorio;
+
+    ProductoControlador(ProductoRepositorio productoRepositorio){
+        this.productoRepositorio = productoRepositorio;
+    }
+
     
+    
+
+
 }
