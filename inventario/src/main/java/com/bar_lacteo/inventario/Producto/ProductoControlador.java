e6a8c860688071a925ac44820f0f3e94f7a0c541
cl.moralesl <cl.moralesl@duocuc.cl>
2025-05-08 17:07:31 -0400

    Se añade lombok para todas las clases y correciones en metodos de Producto

--- inventario/src/main/java/com/bar_lacteo/inventario/Producto/ProductoControlador.java
+++ inventario/src/main/java/com/bar_lacteo/inventario/Producto/ProductoControlador.java
@@ -21,32 +21,8 @@ public class ProductoControlador {
 
     @PostMapping("/registrar")
     public ResponseEntity<?> registrarProducto(@RequestBody Producto producto) {
-        // Validar campos obligatorios
-        if (producto == null || 
-            producto.getNombreProducto() == null || 
-            producto.getNombreProducto().trim().isBlank()) {
-            return ResponseEntity.badRequest().body("El nombre del producto no puede estar vacío");
-        }
-        if (producto.getPrecioUnitario() == null || producto.getPrecioUnitario() <= 0) {
-            return ResponseEntity.badRequest().body("El precio unitario debe ser mayor que cero");
-        }
-        if (producto.getCategoria() == null || producto.getCategoria().getIdCategoria() == null) {
-            return ResponseEntity.badRequest().body("La categoría es obligatoria");
-        }
-
-        // Verificar si ya existe un producto con el mismo nombre
-
-        // Crear nuevo producto
-        Producto nuevoProducto = new Producto();
-        nuevoProducto.setCodigoBarra(producto.getCodigoBarra());
-        nuevoProducto.setNombreProducto(producto.getNombreProducto().trim());
-        nuevoProducto.setDescripcion(producto.getDescripcion() != null ? producto.getDescripcion().trim() : null);
-        nuevoProducto.setPrecioUnitario(producto.getPrecioUnitario());
-        nuevoProducto.setStockMinimo(producto.getStockMinimo());
-        nuevoProducto.setCategoria(producto.getCategoria());
-
-        try {
-            Producto productoGuardado = productoServicio.crearProducto(nuevoProducto);
+         try {
+            Producto productoGuardado = productoServicio.crearProducto(producto);
             return ResponseEntity.ok(productoGuardado);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
