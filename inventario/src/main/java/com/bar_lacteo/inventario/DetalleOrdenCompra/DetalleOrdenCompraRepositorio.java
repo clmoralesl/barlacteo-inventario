7b52889ccc0268b866264c67b489baa00398eedb
Sk <sk@users.noreply.github.com>
2025-05-18 05:19:46 -0400

    Creacion Entidades Estado orden compra y Detalle orden compra

---
+++ inventario/src/main/java/com/bar_lacteo/inventario/DetalleOrdenCompra/DetalleOrdenCompraRepositorio.java
@@ -0,0 +1,7 @@
+package com.bar_lacteo.inventario.DetalleOrdenCompra;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleOrdenCompraRepositorio extends JpaRepository<DetalleOrdenCompra, Long>{
+    
+}
