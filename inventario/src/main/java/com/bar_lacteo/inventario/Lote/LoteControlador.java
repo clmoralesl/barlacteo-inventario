6e56b704f20bb470229f82042c727228a1da66b6
clmoralesl <cl.moralesl@duocuc.cl>
2025-05-22 22:03:11 -0400

    Correcciones menores a código

--- inventario/src/main/java/com/bar_lacteo/inventario/Lote/LoteControlador.java
+++ inventario/src/main/java/com/bar_lacteo/inventario/Lote/LoteControlador.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.*;
 
 @RestController
-@RequestMapping("/apa/lote")
+@RequestMapping("/api/lote")
 
 public class LoteControlador {
 
